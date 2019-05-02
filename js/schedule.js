cefbbab45fbbcaf703f612db5bab6f2103a18514
lintean <904788247@qq.com>
2019-05-02 20:05:00 +0800

    change the host to fix backend easily

--- js/schedule.js
+++ js/schedule.js
@@ -1,7 +1,7 @@
 var container = new Vue({
     el:'#schedule_container',
     data: {
-        host:"110.64.87.155:3000",
+        host:"localhost:3000",
         status: 1, 
         day:null,
         course:null,
